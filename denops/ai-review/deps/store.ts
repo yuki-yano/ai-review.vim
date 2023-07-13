b58219c1c65f9636ea6de238cb54ff93f21baf70
yuki_yano <yuki.ycino@gmail.com>
2023-07-13 11:46:26 +0900

    Add target query for redux in esm.sh

--- denops/ai-review/deps/store.ts
+++ denops/ai-review/deps/store.ts
@@ -1,2 +1,2 @@
-export * as redux from "https://esm.sh/@reduxjs/toolkit@1.9.1"
-export type { PayloadAction } from "https://esm.sh/@reduxjs/toolkit@1.9.1"
+export * as redux from "https://esm.sh/@reduxjs/toolkit@1.9.1?target=es2022"
+export type { PayloadAction } from "https://esm.sh/@reduxjs/toolkit@1.9.1?target=es2022"
