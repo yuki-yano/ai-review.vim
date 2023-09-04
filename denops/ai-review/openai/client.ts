c00c914d406bb4816ad04c310400c46d3dae91e8
kkb0318 <smallobuti@gmail.com>
2023-09-04 21:36:41 +0900

    add documentation for Azure OpenAI support

--- denops/ai-review/openai/client.ts
+++ denops/ai-review/openai/client.ts
@@ -1,60 +1,60 @@
-import { OPENAI_MAX_TOKENS } from "../constant.ts";
+import { OPENAI_MAX_TOKENS } from "../constant.ts"
 import {
   ChatCompletionRequestMessage,
   Configuration,
   OpenAIApi,
   OpenAIStream,
-} from "../deps/ai.ts";
-import { azureConfigSelector, modelSelector } from "../store/config.ts";
+} from "../deps/ai.ts"
+import { azureConfigSelector, modelSelector } from "../store/config.ts"
 
-const ORGANIZATION = Deno.env.get("OPENAI_ORGANIZATION") ?? "";
-const API_KEY = Deno.env.get("OPENAI_API_KEY");
+const ORGANIZATION = Deno.env.get("OPENAI_ORGANIZATION") ?? ""
+const API_KEY = Deno.env.get("OPENAI_API_KEY")
 
 type Client = {
-  completions: typeof completions;
-};
+  completions: typeof completions
+}
 
 async function completions({
   messages,
 }: {
-  messages: Array<ChatCompletionRequestMessage>;
+  messages: Array<ChatCompletionRequestMessage>
 }): Promise<ReadableStream<string>> {
-  const azureConfig = azureConfigSelector();
+  const azureConfig = azureConfigSelector()
   const config: Configuration = azureConfig.use
     ? new Configuration({
-        apiKey: API_KEY,
-        baseOptions: {
-          headers: {
-            "api-key": API_KEY,
-          },
+      apiKey: API_KEY,
+      baseOptions: {
+        headers: {
+          "api-key": API_KEY,
         },
-        basePath: azureConfig.url,
-        defaultQueryParams: new URLSearchParams({
-          "api-version": azureConfig.api_version,
-        }),
-      })
+      },
+      basePath: azureConfig.url,
+      defaultQueryParams: new URLSearchParams({
+        "api-version": azureConfig.api_version,
+      }),
+    })
     : new Configuration({
-        apiKey: API_KEY,
-        organization: ORGANIZATION,
-      });
-  const openai = new OpenAIApi(config);
+      apiKey: API_KEY,
+      organization: ORGANIZATION,
+    })
+  const openai = new OpenAIApi(config)
 
   const res = await openai.createChatCompletion({
     model: modelSelector(),
     max_tokens: OPENAI_MAX_TOKENS,
     stream: true,
     messages,
-  });
+  })
 
-  return OpenAIStream(res).pipeThrough(new TextDecoderStream());
+  return OpenAIStream(res).pipeThrough(new TextDecoderStream())
 }
 
 export function getOpenAiClient(): Client {
   if (API_KEY == null) {
-    throw new Error("OPENAI_API_KEY is not set");
+    throw new Error("OPENAI_API_KEY is not set")
   }
 
   return {
     completions,
-  };
+  }
 }
--- denops/ai-review/store/config.ts
+++ denops/ai-review/store/config.ts
@@ -1,25 +1,25 @@
-import { PayloadAction, redux } from "../deps/store.ts";
-import { store } from "./index.ts";
-const { createSlice } = redux;
+import { PayloadAction, redux } from "../deps/store.ts"
+import { store } from "./index.ts"
+const { createSlice } = redux
 
 export type Config = {
-  log_dir: string;
+  log_dir: string
   chat_gpt: {
-    model: string;
-    azure: AzureConfig;
+    model: string
+    azure: AzureConfig
     // Not use TypeScript
     // requests: Array
-  };
-};
+  }
+}
 type AzureConfig = {
-  use: boolean;
-  url: string;
-  api_version: string;
-};
+  use: boolean
+  url: string
+  api_version: string
+}
 
 type ConfigState = {
-  config: Config;
-};
+  config: Config
+}
 
 const configInitialState: ConfigState = {
   config: {
@@ -33,39 +33,39 @@ const configInitialState: ConfigState = {
       },
     },
   },
-};
+}
 
 export const configSlice = createSlice({
   name: "config",
   initialState: configInitialState,
   reducers: {
     config: (state, action: PayloadAction<ConfigState>) => {
-      const input = action.payload.config;
-      validate(input);
-      state.config = input;
+      const input = action.payload.config
+      validate(input)
+      state.config = input
     },
   },
-});
+})
 
 const validate = (config: Config) => {
   if (config.chat_gpt.model === "") {
-    throw new Error("chatGPT model is empty");
+    throw new Error("chatGPT model is empty")
   }
-  const azureConfig = config.chat_gpt.azure;
+  const azureConfig = config.chat_gpt.azure
   if (azureConfig.use) {
-    console.log(azureConfig);
+    console.log(azureConfig)
     if (azureConfig.url === "") {
-      throw new Error("chatGPT azure.url is empty");
+      throw new Error("chatGPT azure.url is empty")
     }
     if (azureConfig.api_version === "") {
-      throw new Error("chatGPT azure.api_version is empty");
+      throw new Error("chatGPT azure.api_version is empty")
     }
   }
-};
+}
 
 export const modelSelector = (): string =>
-  store.getState().config.config.chat_gpt.model;
+  store.getState().config.config.chat_gpt.model
 export const azureConfigSelector = (): AzureConfig =>
-  store.getState().config.config.chat_gpt.azure;
+  store.getState().config.config.chat_gpt.azure
 export const logDirSelector = (): string =>
-  store.getState().config.config.log_dir;
+  store.getState().config.config.log_dir
