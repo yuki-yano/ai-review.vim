2bfdf5f823be7f0d609b64a028e2fb6e58404868
kkb0318 <smallobuti@gmail.com>
2023-09-04 10:31:57 +0900

    add azure open ai support

--- denops/ai-review/store/config.ts
+++ denops/ai-review/store/config.ts
@@ -1,28 +1,39 @@
-import { PayloadAction, redux } from "../deps/store.ts"
-import { store } from "./index.ts"
-const { createSlice } = redux
+import { PayloadAction, redux } from "../deps/store.ts";
+import { store } from "./index.ts";
+const { createSlice } = redux;
 
 export type Config = {
-  log_dir: string
+  log_dir: string;
   chat_gpt: {
-    model: string
+    model: string;
+    azure: AzureConfig;
     // Not use TypeScript
     // requests: Array
-  }
-}
+  };
+};
+type AzureConfig = {
+  use: boolean;
+  url: string;
+  api_version: string;
+};
 
 type ConfigState = {
-  config: Config
-}
+  config: Config;
+};
 
 const configInitialState: ConfigState = {
   config: {
     log_dir: "",
     chat_gpt: {
       model: "",
+      azure: {
+        use: false,
+        url: "",
+        api_version: "",
+      },
     },
   },
-}
+};
 
 export const configSlice = createSlice({
   name: "config",
@@ -30,10 +41,14 @@ export const configSlice = createSlice({
   reducers: {
     config: (state, action: PayloadAction<ConfigState>) => {
       // TODO: validate config
-      state.config = action.payload.config
+      state.config = action.payload.config;
     },
   },
-})
+});
 
-export const modelSelector = (): string => store.getState().config.config.chat_gpt.model
-export const logDirSelector = (): string => store.getState().config.config.log_dir
+export const modelSelector = (): string =>
+  store.getState().config.config.chat_gpt.model;
+export const azureConfigSelector = (): AzureConfig =>
+  store.getState().config.config.chat_gpt.azure;
+export const logDirSelector = (): string =>
+  store.getState().config.config.log_dir;
